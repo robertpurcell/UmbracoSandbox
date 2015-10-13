02cea1cf8f62b25e49c82a7ccf829682d2646f32
Rob Purcell <rpurcell1987@gmail.com>
2015-10-14 00:50:31 +0100

    Robots and unit test

--- UmbracoSandbox.Web.Tests/UnitTest1.cs
+++ UmbracoSandbox.Web.Tests/UnitTest1.cs
@@ -8,6 +8,9 @@
         [TestMethod]
         public void TestMethod1()
         {
+            var expected = 5;
+            var actual = 5;
+            Assert.AreEqual(expected, actual);
         }
     }
 }
