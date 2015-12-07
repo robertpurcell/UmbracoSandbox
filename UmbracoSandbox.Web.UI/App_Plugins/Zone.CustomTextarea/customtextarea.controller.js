f9e16a693c84fabce911d156d9f33943d90281fe
Rob Purcell <rpurcell@thisiszone.com>
2015-12-07 18:30:11 +0000

    Custom Textarea and Angular patch

---
+++ UmbracoSandbox.Web.UI/App_Plugins/Zone.CustomTextarea/customtextarea.controller.js
@@ -0,0 +1,8 @@
+﻿angular.module("umbraco").controller("Zone.CustomTextareaController",
+ function ($scope) {
+     // Check if a value is set.  If not then use the default settings
+    $scope.model.rows = $scope.model.config.rows;
+    if ($scope.model.rows === null || $scope.model.rows <= 0) {
+        $scope.model.rows = 10;
+    }
+ });
